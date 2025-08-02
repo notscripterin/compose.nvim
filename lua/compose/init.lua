d16a582b5521f68042245d837339b79f42e7c462
MRINMOY MONDAL <mm2922894@gmail.com>
2025-08-02 19:57:01 +0530

    added subcommands support

--- lua/compose/init.lua
+++ lua/compose/init.lua
@@ -6,7 +6,9 @@ local function trim(s)
 	return s:gsub("^%s*(.-)%s*$", "%1")
 end
 
-function M.init() end
+function M.init()
+	log.info("compose init")
+end
 
 function M.sync() end
 
@@ -15,9 +17,24 @@ function M.run() end
 function M.setup(opts)
 	opts = opts or {}
 
-	vim.api.nvim_create_user_command("Compose init", M.init, {})
-	vim.api.nvim_create_user_command("Compose sync", M.sync, {})
-	vim.api.nvim_create_user_command("Compose run", M.run, {})
+	vim.api.nvim_create_user_command("Compose", function(_opts)
+		local subcmd = _opts.args
+
+		if subcmd == "init" then
+			M.init()
+		elseif subcmd == "sync" then
+			M.sync()
+		elseif subcmd == "run" then
+			M.run()
+		else
+			log.error("Unknown argument '" .. subcmd .. "'")
+		end
+	end, {
+		nargs = 1,
+		complete = function()
+			return { "init", "sync", "run" }
+		end,
+	})
 end
 
 return M
